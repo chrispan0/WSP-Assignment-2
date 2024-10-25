0f35ce1c9ceff020c60ce39ff14bd7063a890bdb
chrispan-i3 <github@chrispanetta.com>
2024-10-25 18:06:50 -0400

    made changes to the ejs files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+router.get("/project", function (req, res, next) {
+  res.render("project", { title: "Project" });
+});
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About" });
+});
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
+});
+router.get("/github", function (req, res, next) {
+  res.render("github", { title: "GitHub" });
 });
 
 module.exports = router;
