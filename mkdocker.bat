0c09c176e5ce68abe84e9a25a483fd73b500a826
Chris Panetta <cv.panetta@gmail.com>
2024-10-25 17:47:03 -0400

    removes previous image and changed port

--- mkdocker.bat
+++ mkdocker.bat
@@ -1,3 +1,5 @@
+docker stop wsp-assignment-2
+docker rm wsp-assignment-2
 cd C:\Users\Administrator\Documents\WSP Assignment 2 Server\WSP-Assignment-2
 docker build -t wsp-assignment-2 .
-docker run -p 1337:3000 --name portfolio-container wsp-assignment-2
+docker run -p 1337:4000 --name wsp-assignment-2 wsp-assignment-2:latest
