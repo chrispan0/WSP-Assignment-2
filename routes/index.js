56ab6860efa10b9463e71efba5cebcd5f0ab01d4
chrispan-i3 <github@chrispanetta.com>
2024-10-30 10:16:55 -0400

    Updated CSS to be more modern, added features, removed comments

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-// Home page route
 router.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-// About page route
 router.get("/about", (req, res) => {
   res.render("about", { title: "About Me" });
 });
 
-// Projects page route
 router.get("/projects", (req, res) => {
   res.render("projects", { title: "Projects" });
 });
 
-// Contact page route
 router.get("/contact", (req, res) => {
   res.render("contact", { title: "Contact" });
 });
 
-// GitHub page route
 router.get("/github", (req, res) => {
   res.render("github", { title: "GitHub" });
 });
